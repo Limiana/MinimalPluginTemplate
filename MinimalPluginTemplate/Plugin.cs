ec1a75c35634186d40efb103d6ef9bf4130b810b
Eternita-S <lysithea.star@gmail.com>
2021-09-22 18:43:30 +0300

    Add project files.

---
+++ MinimalPluginTemplate/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using Dalamud.Plugin;
+using System;
+
+namespace MinimalPluginTemplate
+{
+    public class Plugin : IDalamudPlugin
+    {
+        public string Name => "Name of the plugin";
+
+        public Plugin(DalamudPluginInterface pluginInterface)
+        {
+            pluginInterface.Create<Svc>();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
